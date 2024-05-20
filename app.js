1bfe12283e09c8268308b5093242f5c81cee3392
Nastya-2024 <nast.marchenko.2008@mail.ru>
2024-05-20 17:52:06 +0300

    коллекции для mongodb

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const apiRouter = require('./routes/apiRouter');
+const pagesRouter = require('./routes/pages');
+
+const connectToDatabase = require('./database/connect');
+const cors = require('./middlewares/cors');
+
+const cookieParser = require("cookie-parser");
+
 const app = express();
+const PORT = 3001;
 
-const PORT = 3005;
+connectToDatabase();
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+// app.js
+// Импорты и инициализация приложения
+
+app.use(
+  cors,
+  cookieParser(),
+  bodyParser.json(),
+  pagesRouter,
+  apiRouter, // Добавляем
+  express.static(path.join(__dirname, "public")),
+);
+
+// Запуск приложения
+
+app.listen(PORT);
